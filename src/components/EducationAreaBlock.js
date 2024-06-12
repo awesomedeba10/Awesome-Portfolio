894f4edd028c7f56103bfb3fbc0672108321ae6e
Debanjan Ganguly <debanjan.ganguly@navi.com>
2024-06-13 03:23:07 +0530

    fixeed key error

--- src/components/EducationAreaBlock.js
+++ src/components/EducationAreaBlock.js
@@ -5,15 +5,15 @@ export default function EducationAreaBlock() {
     return (
         <div className="d-flex flex-wrap gap-4 align-items-center justify-content-between">
             {educationData.map((list, index) => (
-            <div className="edu-text-box">
-                <span className="mb-3 d-block fs-seven fw_300 n06">
-                    {list.yearFrom} - {list.yearTo}
-                </span>
-                <span className="fs20 mb-2 d-block n0-color">
-                    {list.degree} in {list.stream}
-                </span>
-                <span className="d-block fs-seven fw_300 n06">{list.institutionName}</span>
-            </div>
+                <div className="edu-text-box" key={index}>
+                    <span className="mb-3 d-block fs-seven fw_300 n06">
+                        {list.yearFrom} - {list.yearTo}
+                    </span>
+                    <span className="fs20 mb-2 d-block n0-color">
+                        {list.degree} in {list.stream}
+                    </span>
+                    <span className="d-block fs-seven fw_300 n06">{list.institutionName}</span>
+                </div>
             ))}
         </div>
     );
